9a0bba00c1eed0a2e60126ce290eed8b52a93399
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2023-05-24 02:10:52 +0200

    change equal to strict equality

--- deobfuscator.js
+++ deobfuscator.js
@@ -8,11 +8,11 @@ const output = babel.transformSync(code, {
 				visitor: {
 					BinaryExpression(path) {
 						if (path.node.left.type.match(pattern) && path.node.right.type.match(pattern)) {
-							if (path.node.left.type == "NumericLiteral" && path.node.right.type == "NumericLiteral")
+							if (path.node.left.type === "NumericLiteral" && path.node.right.type === "NumericLiteral")
 								path.node.type = "NumericLiteral";
 							else
 								path.node.type = "StringLiteral";
-							path.node.value = eval(path.node.left.extra.raw+path.node.operator+path.node.right.extra.raw);
+							path.node.value = eval(path.node.left.extra.raw + path.node.operator + path.node.right.extra.raw);
 						}
 					}
 				}
