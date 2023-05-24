2de6aafaec1beeb323bf73db97b473913dd6e23b
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2023-05-24 18:40:49 +0200

    use babel traverse for visiting the tree

--- deobfuscator.js
+++ deobfuscator.js
@@ -1,25 +1,28 @@
-var babel = require("@babel/core");
-const code = "var a = 5+5";
-const output = babel.transformSync(code, {
-	plugins: [
-		function evaluateStaticExpression() {
-			const pattern = /(Numeric|String)Literal/;
-			return {
-				visitor: {
-					BinaryExpression(path) {
-						if (path.node.left.type.match(pattern) && path.node.right.type.match(pattern)) {
-							if (path.node.left.type === "NumericLiteral" && path.node.right.type === "NumericLiteral")
-								path.node.type = "NumericLiteral";
-							else
-								path.node.type = "StringLiteral";
-							path.node.value = eval(path.node.left.extra.raw + path.node.operator + path.node.right.extra.raw);
-						}
-					}
-				}
-				
+const babel = require("@babel/core");
+const generate = require("@babel/generator").default;
+
+const code = 
+`var a = 5 + 5 + 3 + 4;
+`;
+
+const ast = babel.parse(code);
+
+const pattern = /(Numeric|String)Literal/;
+
+babel.traverse(ast, {
+	exit(path) {
+		if (path.node.type === "BinaryExpression") {
+			if (path.node.left.type.match(pattern) && path.node.right.type.match(pattern)) {
+				if (path.node.left.type === "NumericLiteral" && path.node.right.type === "NumericLiteral")
+					path.node.type = "NumericLiteral";
+				else
+					path.node.type = "StringLiteral";
+				path.node.value = eval(path.node.left.value + path.node.operator + path.node.right.value);
 			}
 		}
-	]
+	}
+
 });
 
-console.log(output.code)
+const output = generate(ast);
+console.log(output.code);
