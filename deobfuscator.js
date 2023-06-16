bee7c7674bba412a22223102ec1dfed2df4190c9
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2023-06-16 20:33:13 +0200

    add string property functions

--- deobfuscator.js
+++ deobfuscator.js
@@ -62,6 +62,77 @@ function evaluateUnaryExpression(operator, prefix, value) {
 	}
 }
 
+function evaluateProperty(object, property, arguments) {
+	const strType = "StringLiteral";
+	const numType = "NumericLiteral";
+	const boolType = "BooleanLiteral";
+	const arrType = "ArrayExpression";
+	switch (property) {
+
+		//String property
+	
+		case "at": return [strType, object.at(arguments[0].value)];
+		case "charAt": return [strType, object.charAt(arguments[0].value)];
+		case "charCodeAt": return [numType, object.charCodeAt(arguments[0].value)];
+		case "codePointAt": return [numType, object.codePointAt(arguments[0].value)];
+		case "concat": return [strType, object.concat(arguments[0].value)];
+		case "endsWith":  if (arguments.length == 1) return object.endsWith(arguments[0].value); else return object.endsWith(arguments[0].value, arguments[1].value);
+		case "fromCharCode": {
+			let resultString;
+			for(arg in arguments) {
+				resultString += object.fromCharCode(arg)
+			}
+			return resultString;
+		}
+		case "fromCodePoint": {
+			let resultString;
+			for(arg in arguments) {
+				resultString += object.fromCodePoint(arg);
+			}
+			return resultString;
+		}
+		case "includes": if (arguments.length == 1) return [boolType, object.includes(arguments[0].value)]; else return [boolType, object.includes(arguments[0].value, arguments[1].value)];
+		case "indexOf": if (arguments.length == 1) return [numType, object.indexOf(arguments[0].value)]; else return [numType, object.indexOf(arguments[0].value, arguments[1].value)];
+		case "isWellFormed": return object.isWellFormed(arguments[0].value);
+		case "lastIndexOf": if (arguments.length == 1) return [numType, object.lastIndexOf(arguments[0].value)]; else return [numType, object.lastIndexOf(arguments[0].value, arguments[1].value)];
+		case "localeCompare": {
+			switch (arguments.length) {
+				case 1: return object.localeCompare(arguments[0].value);
+				case 2: return object.localeCompare(arguments[0].value, arguments[1].value);
+				case 3: return object.localeCompare(arguments[0].value, arguments[1].value, arguments[2].value);
+			}
+		}
+		case "match": return [arrType, object.match(arguments[0].value)];
+		case "matchAll": return [arrType, object.matchAll(arguments[0].value)];
+		case "normalize": if (arguments.length == 0) return [strType, object.normalize()]; else return [strType, object.lastIndexOf(arguments[0].value)];
+		case "padEnd": if (arguments.length == 1) return [strType, object.padEnd(arguments[0].value)]; else return [strType, object.padEnd(arguments[0].value, arguments[1].value)];
+		case "padStart": if (arguments.length == 1) return [strType, object.padStart(arguments[0].value)]; else return [strType, object.padStart(arguments[0].value, arguments[1].value)];
+		case "raw": return;
+		case "repeat": return [strType, object.repeat(arguments[0].value)];
+		case "replace": return [strType, object.replace(arguments[0].value, arguments[1].value)];
+		case "replaceAll": return [strType, object.replaceAll(arguments[0].value, arguments[1].value)];
+		case "search": return [numType, object.search(arguments[0].value)];
+		case "slice": if (arguments.length == 1) return [strType, object.slice(arguments[0].value)]; else return [strType, object.slice(arguments[0].value, arguments[1].value)];
+		case "split": if (arguments.length == 1) return [strType, object.split(arguments[0].value)]; else return [strType, object.split(arguments[0].value, arguments[1].value)];
+		case "startsWith": if (arguments.length == 1) return [boolType, object.startsWith(arguments[0].value)]; else return [boolType, object.startsWith(arguments[0].value, arguments[1].value)];
+		case "substring": if (arguments.length == 1) return [strType, object.substring(arguments[0].value)]; else return [strType, object.substring(arguments[0].value, arguments[1].value)];
+		case "toLocaleLowerCase": if (arguments.length == 0) return [strType, object.toLocaleLowerCase()]; else return [strType, object.toLocaleLowerCase(arguments[0].value)];
+		case "toLocaleUpperCase": if (arguments.length == 0) return [strType, object.toLocaleUpperCase()]; else return [strType, object.toLocaleUpperCase(arguments[0].value)];
+		case "toLowerCase": return object.toLowerCase();
+		case "toString": return object.toString();
+		case "toUpperCase": return object.toUpperCase();
+		case "toWellFormed":return object.toWellFormed();
+		case "trim": return object.trim();
+		case "trimEnd": return object.trimEnd();
+		case "trimRight": return object.trimEnd();
+		case "trimStart": return object.trimStart();
+		case "trimLeft": return object.trimStart();
+		case "valueOf": return object.valueOf();
+		
+
+	}
+}
+
 const code = 
 `
 function add() {
@@ -77,6 +148,8 @@ var g = !true;
 var h = +!+[];
 var i = [+!+[]]+[+[]];
 var j = 1+[0];
+var k = "hello";
+k.replace('h','hh');
 `;
 
 var ast = babel.parse(code);
@@ -163,6 +236,17 @@ babel.traverse(ast, {
 				delete path.node.operator
 				delete path.node.prefix;
 				delete path.node.argument;
+			}
+		}
+
+		if (path.node.type == "CallExpression") {
+			let value;
+			if (path.node.callee.type == "MemberExpression") {
+				[type, value] = evaluateProperty(path.node.callee.object.value, path.node.callee.property.name, path.node.arguments);
+				path.node.type = type;
+				delete path.node.callee;
+				delete path.node.arguments;
+				path.node.value = value;					
 			}
 		}
 	}
