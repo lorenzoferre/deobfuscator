1df4a918d5dee3dfdf8585e93d8c7272f5be8a58
lorenzoferre <lorenzo.ferretti4@gmail.com>
2023-08-14 19:51:44 +0200

    create initialize function

--- src/deobfuscator.js
+++ src/deobfuscator.js
@@ -5,18 +5,12 @@ const generate = _generate.default;
 
 import deleteIrrelevantFields from "./delete-irrelevant-fields.js";
 import deleteUnreachableFunctions from "./delete-unreachable-functions.js";
-import renameVariableSameScope from "./rename-variables-same-scope.js";
-import defeatingMapArrayMapping from "./defeating-map-array-mappings.js";
-import costantFolding from "./costant-folding.js";
-import evaluate from "./evaluate.js";
-import defeatingStringArrayMapping from "./defeating-string-array-mappings.js";
-import transformBracketToDot from "./transform-from-brackets-to-dots.js";
-import evaluateConditionalStatement from "./evaluate-conditional-statements.js";
-import replaceOutermostIife from "./replace-outermost-iifes.js";
+import deobfuscate from "./deobfuscate.js";
 
 
 // var ast;
 
+/*
 function unflatteningSwitch(path) {
 	let loopStmt = path.node;
 	let bodyBlock = loopStmt.body;
@@ -40,57 +34,19 @@ function unflatteningSwitch(path) {
 	let discriminantValue = binding.path.node.init.value;
   
 	// it must be finished
-}
+}*/
 
-export function deobfuscate(code) {
+function initialize() {
 	let ast = parse(code);
 	deleteUnreachableFunctions(ast);
 	deleteIrrelevantFields(ast);
-	traverse(ast, {
-		exit(path) {
-			// costant propagation
-			if (t.isVariableDeclarator(path)) {
-				renameVariableSameScope(path);
-				defeatingMapArrayMapping(path);
-				costantFolding(path);
-			}
-			// evaluate expression with constant values
-			if (t.isBinaryExpression(path) ||
-				t.isUnaryExpression(path) ||
-				t.isLogicalExpression(path)) {
-				evaluate(path);
-			}
-			// defeating literal array mappings
-			if (t.isMemberExpression(path)) {
-				defeatingStringArrayMapping(path);
-			}
-			// transform bracket notation into dot notation
-			if (t.isCallExpression(path)) {
-				// ADD function for reversing jsfuck notation with vm module
-				transformBracketToDot(path);
-				evaluate(path);
-			}
-			// evaluate if and ternary statementss
-			if (t.isIfStatement(path) || t.isConditionalExpression(path)) {
-				evaluateConditionalStatement(path);
-			}
-			// control flow unflattening
-			/*
-			if (t.isWhileStatement(path) || t.isForStatement(path)) {
-				unflatteningSwitch(path);
-			}*/
-			// replace outermost iife with all code inside it
-			if (t.isExpressionStatement(path)) {
-				replaceOutermostIife(path);
-			}
-			// removes empty statement
-			if (t.isEmptyStatement(path.node)) {
-				path.remove();
-			}
-		}
-	
-	});
+	return ast;
+}
 
+ 
+export function processDeobfuscation(code) {
+	let ast = initialize();
+	deobfuscate(ast);
 	const output = generate(ast, {comments: false});
 	return output.code
 }
@@ -100,5 +56,5 @@ const code =
 var a = "a|b|c";
 console.log(a.split("|"));
 `;
-const cleanCode = deobfuscate(code);
+const cleanCode = processDeobfuscation(code);
 console.log(cleanCode);
