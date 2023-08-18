4965b66532f45079b6bb5c8d922beeeff44650d0
lorenzoferre <lorenzo.ferretti4@gmail.com>
2023-08-18 19:00:29 +0200

    fix untracked files

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Deobfuscator from "./deobfuscator.js"
-const code =
-`
-console.log("1");
-`;
-
-const deobfuscator = new Deobfuscator(code);
-const deobfuscatedCode = deobfuscator.deobfuscate();
-console.log(deobfuscatedCode);
