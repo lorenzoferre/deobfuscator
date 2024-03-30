e0f6fa959931e044ef55bff968d490b08225ba67
lorenzoferre <lorenzo.ferretti4@gmail.com>
2024-03-30 17:28:20 +0100

    wip control flow unflattening

--- src/techniques/statics/control-flow-unflattening.js
+++ src/techniques/statics/control-flow-unflattening.js
@@ -1,12 +1,16 @@
 import { setChanged } from "../../utils/util.js";
+import vm from "vm";
 
-function buildSwitchBlocks(switchStatement) {
-  let switchBlocks = {};
+import _generate from "@babel/generator";
+const generate = _generate.default;
+
+function buildSwitchCases(switchStatement) {
+  let switchCases = {};
   for (const switchCase of switchStatement.cases) {
     const key = switchCase.test.value || switchCase.test.name;
-    switchBlocks[key] = switchCase.consequent;
+    switchCases[key] = switchCase.consequent;
   }
-  return switchBlocks;
+  return switchCases;
 }
 
 function getNextValue(statement, name, t) {
@@ -24,36 +28,79 @@ export default function (babel) {
   return {
     name: "control-flow-unflattening",
     visitor: {
-      "WhileStatement|DoWhileStatement"(path) {
-        const { node, scope } = path;
-        const { body } = node.body;
-        if (!body) return;
-        const switchStatement = body[0];
-        if (!t.isSwitchStatement(switchStatement)) return;
-        const { discriminant } = switchStatement;
-        const binding = scope.getBinding(discriminant.name);
-        if (!binding) return;
-        if (!t.isLiteral(binding.path.node.init)) return;
-        const { name } = binding.path.node.id;
-        let { value } = binding.path.node.init;
-        const switchBlocks = buildSwitchBlocks(switchStatement);
-        let stuffInOrder = [];
-        for (let i = 0; i < Object.keys(switchBlocks).length; i++) {
-          let blocksByCase = switchBlocks[value];
-          if (!blocksByCase) return;
-          for (const block of blocksByCase) {
-            if (!t.isBlockStatement(block)) continue;
-            for (let statement of block.body) {
-              stuffInOrder.push(statement);
-              const nextValue = getNextValue(statement, name, t);
-              if (!nextValue) continue;
-              value = nextValue;
-              blocksByCase = switchBlocks[value];
+      "WhileStatement|DoWhileStatement|ForStatement": {
+        exit(path) {
+          const { node, scope } = path;
+          const { body } = node.body;
+          if (!body) return;
+          const switchStatement = body[0];
+          if (!t.isSwitchStatement(switchStatement)) return;
+          const { discriminant } = switchStatement;
+          const binding = scope.getBinding(discriminant.name);
+          if (!binding) return;
+          if (!t.isLiteral(binding.path.node.init)) return;
+          let stuffInOrder = [];
+          let doHaveToSearch = true;
+          let maxIterations = 1e3;
+          let iteration = 0;
+          let possibleNextValue;
+          let context = {};
+          const { name } = binding.path.node.id;
+          let { value } = binding.path.node.init;
+          context[name] = value;
+          const switchCases = buildSwitchCases(switchStatement);
+          outerLoop: while (doHaveToSearch && iteration < maxIterations) {
+            let switchCaseNodes = switchCases[value];
+            // following switch case test values
+            for (const switchCaseNode of switchCaseNodes) {
+              if (t.isContinueStatement(switchCaseNode)) continue outerLoop;
+              if (t.isBreakStatement(switchCaseNode)) break;
+              if (!t.isBlockStatement(switchCaseNode)) {
+                stuffInOrder.push(switchCaseNode);
+                possibleNextValue = getNextValue(switchCaseNode, name, t);
+                if (!possibleNextValue) continue;
+                value = possibleNextValue;
+                context[name] = value;
+              } else {
+                for (let statement of switchCaseNode.body) {
+                  stuffInOrder.push(statement);
+                  if (!t.isExpressionStatement(statement)) continue;
+                  const { expression } = statement;
+                  if (!t.isAssignmentExpression(expression)) continue;
+                  const { left } = expression;
+                  const { right } = expression;
+                  if (t.isLiteral(right)) context[left.name] = right.value;
+                  if (t.isBinaryExpression(right)) {
+                    vm.createContext(context);
+                    context[left.name] = vm.runInContext(generate(right).code, context);
+                  }
+                  possibleNextValue = getNextValue(statement, name, t);
+                  if (!possibleNextValue) continue;
+                  value = possibleNextValue;
+                  context[name] = value;
+                }
+              }
             }
+
+            for (const statement of body.slice(1)) {
+              if (t.isBreakStatement(statement)) {
+                doHaveToSearch = false;
+                break;
+              }
+              if (t.isContinueStatement(statement)) continue outerLoop;
+              stuffInOrder.push(statement);
+            }
+            // TODO evaluate the test of the loop with vm module
+            vm.createContext(context);
+            try {
+              let testOuterLoop = vm.runInContext(generate(node.test).code, context);
+              if (!testOuterLoop) doHaveToSearch = false;
+            } catch {}
+            iteration++;
           }
-        }
-        path.replaceWithMultiple(stuffInOrder);
-        setChanged(true);
+          path.replaceWithMultiple(stuffInOrder);
+          setChanged(true);
+        },
       },
     },
   };
