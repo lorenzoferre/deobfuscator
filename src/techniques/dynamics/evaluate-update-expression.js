dc42de30638b121c2d6d568200c69f0cc6b2a92e
lorenzoferre <lorenzo.ferretti4@gmail.com>
2024-09-14 01:48:27 +0200

    fix evaluation of update expressions

--- src/techniques/dynamics/evaluate-update-expression.js
+++ src/techniques/dynamics/evaluate-update-expression.js
@@ -1,63 +1,91 @@
-import { setChanged } from "../../utils/util.js";
+import { context, setChanged } from "../../utils/util.js";
 import _generate from "@babel/generator";
 const generate = _generate.default;
 import vm from "vm";
 
 export default function (babel) {
   const { types: t } = babel;
-  const context = vm.createContext();
-  let scopeUid;
+  const functionMap = {};
+  const callStack = [];
+
+  function processVariableDeclarators(path) {
+    const { node } = path;
+    const { id, init } = node;
+    const { name } = id;
+
+    // mapping names to values
+    if (!init) {
+      context[name] = undefined;
+    } else {
+      if (!t.isLiteral(init)) return;
+      const { value } = init;
+      context[name] = value;
+    }
+  }
+
+  function processExpressionStatement(path) {
+    const { node, scope } = path;
+    const { expression } = node;
+
+    if (t.isUpdateExpression(expression) || t.isAssignmentExpression(expression)) {
+      const expressionCode = generate(node).code;
+      let variableName;
+      if (t.isUpdateExpression(expression)) {
+        variableName = expression.argument.name;
+      } else if (t.isAssignmentExpression(expression)) {
+        if (!t.isIdentifier(expression.left)) return;
+        variableName = expression.left.name;
+      }
+      if (!context.hasOwnProperty(variableName)) return;
+      const value = vm.runInContext(expressionCode, context);
+      if (value && typeof value !== "function") {
+        context[variableName] = value;
+        const binding = scope.getBinding(variableName);
+        if (!binding) return;
+        for (const referencePath of binding.referencePaths) {
+          referencePath.replaceWith(t.valueToNode(value));
+        }
+        path.remove();
+      }
+    }
+  }
+
+  function traverseFunction(name) {
+    const functionPath = functionMap[name];
+    if (functionPath) {
+      functionPath.traverse({
+        ExpressionStatement(path) {
+          processExpressionStatement(path);
+        },
+      });
+    }
+  }
 
   return {
     name: "evaluate-update-expression",
     visitor: {
-      VariableDeclarator: {
+      Program: {
         enter(path) {
-          const { node } = path;
-          if (!node.init) return;
-          const { scope } = path;
-          scopeUid = scope.uid;
-          const declaration = generate(node).code;
-          try {
-            vm.runInContext(declaration, context);
-          } catch {}
-        },
-      },
-      ExpressionStatement: {
-        enter(path) {
-          const { node } = path;
-          const { scope } = path;
-          if (scopeUid !== scope.uid) return;
-          const { expression } = node;
-          if (t.isUpdateExpression(expression) || t.isAssignmentExpression(expression)) {
-            const expressionCode = generate(node).code;
-            try {
-              const value = vm.runInContext(expressionCode, context);
-              if (value) {
-                path.remove();
-                setChanged(true);
-              }
-            } catch {}
-          }
-        },
-      },
-      Identifier: {
-        enter(path) {
-          const { node } = path;
-          const { scope } = path;
-          if (scopeUid !== scope.uid) return;
-          const { parent } = path;
-          if (t.isUpdateExpression(parent) || t.isAssignmentExpression(parent)) return;
-          const parentFather = path.parentPath.parent;
-          if (!t.isExpressionStatement(parentFather)) return;
-          if (context.hasOwnProperty(node.name)) {
-            if (
-              typeof context[node.name] === "function" ||
-              typeof context[node.name] === "object"
-            )
-              return;
-            path.replaceWith(t.valueToNode(context[node.name]));
-            setChanged(true);
+          path.traverse({
+            FunctionDeclaration(path) {
+              const { node } = path;
+              const { id } = node;
+              functionMap[id.name] = path;
+            },
+            CallExpression(path) {
+              const { node } = path;
+              const { callee } = node;
+              const { name } = callee;
+              callStack.push(name);
+            },
+            VariableDeclarator(path) {
+              processVariableDeclarators(path);
+            },
+          });
+
+          while (callStack.length > 0) {
+            const currentCall = callStack.shift();
+            traverseFunction(currentCall);
           }
         },
       },
